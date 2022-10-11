48447333d0b0867556eae2fbb250df71fefd399a
HwangTaeGyeong <htk5205@naver.com>
2022-10-11 16:00:49 +0900

    Spring DI 실습(생성자) - MessageSendService

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/service/MessageSendService.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/service/MessageSendService.java
@@ -3,19 +3,14 @@ package com.nhnacademy.edu.springframework.messagesender.service;
 import com.nhnacademy.edu.springframework.messagesender.User;
 
 public class MessageSendService {
-//    private final MessageSender messageSender;
-    private SmsMessageSender smsMessageSender;
-    private EmailMessageSender emailMessageSender;
+    private final MessageSender messageSender;
 
-    public MessageSendService(SmsMessageSender smsMessageSender, EmailMessageSender emailMessageSender) {
-        this.smsMessageSender = smsMessageSender;
-        this.emailMessageSender = emailMessageSender;
+    public MessageSendService(MessageSender messageSender) {
+        this.messageSender = messageSender;
     }
 
     public void doSendMessage() {
-
-        smsMessageSender.sendMessage(new User("nhnacademy.com","010-1234-5678"),"message");
-        emailMessageSender.sendMessage(new User("nhnacademy.com","010-1234-5678"),"message");
+        messageSender.sendMessage(new User("nhnacademy.com","010-1234-5678"),"message");
     }
 
 }
