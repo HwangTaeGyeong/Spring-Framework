d30bc5709b9ee1ae1faef784857cf878c1e8ba5f
HwangTaeGyeong <htk5205@naver.com>
2022-10-11 10:45:37 +0900

    자바 다형성 Main, User 생성

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/Main.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/Main.java
@@ -1,2 +1,15 @@
-package com.nhnacademy.edu.springframework.messagesender;public class Main {
+package com.nhnacademy.edu.springframework.messagesender;
+
+public class Main {
+    public static void main(String args[]) {
+        Main main = new Main();
+        User user = new User("nhnacademy.com", "010-1234-5678");
+        main.sendSmsMessage(user, "message");
+    }
+    public void sendSmsMessage(User user, String message) {
+        System.out.println( "SMS Message Sent to ${" + user.phoneNumber + "} : ${" + message + "}");
+    }
+    public void sendEmailMessage(User user, String message) {
+        System.out.println( "Email Message Sent ${" + user.email + "} : ${" + message + "}");
+    }
 }
