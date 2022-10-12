fc415dcb99514b794472db7c1695024d48201fe7
HwangTaeGyeong <htk5205@naver.com>
2022-10-12 13:07:41 +0900

    Spring DI 실습(annotation) - Value

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/service/MessageSendService.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/service/MessageSendService.java
@@ -3,6 +3,7 @@ package com.nhnacademy.edu.springframework.messagesender.service;
 import com.nhnacademy.edu.springframework.messagesender.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 
 public class MessageSendService {
     private MessageSender messageSender;
@@ -11,12 +12,16 @@ public class MessageSendService {
 //        System.out.println("-------Constructor Injection---------");
 //        this.messageSender = messageSender;
 //    }
+
+    @Value("${from}")
+    private String from;
     @Autowired
     public  void setKoreanGreeter(@SMS("koreanGreeter") MessageSender messageSender) {
         System.out.println("setMessageSender invoked!!");
         this.messageSender = messageSender;
     }
     public void doSendMessage() {
+        System.out.println("From: " + this.from);
         messageSender.sendMessage(new User("nhnacademy.com","010-1234-5678"),"message");
     }
 }
