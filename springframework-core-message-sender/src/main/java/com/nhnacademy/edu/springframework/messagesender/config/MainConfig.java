0a9516525565f6b1d439db0409431ab00bd72316
HwangTaeGyeong <htk5205@naver.com>
2022-10-12 14:22:34 +0900

    Spring JavaConfig

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/config/MainConfig.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/config/MainConfig.java
@@ -1,9 +1,22 @@
 package com.nhnacademy.edu.springframework.messagesender.config;
 
+import com.nhnacademy.edu.springframework.messagesender.service.EmailMessageSender;
+import com.nhnacademy.edu.springframework.messagesender.service.MessageSender;
+import com.nhnacademy.edu.springframework.messagesender.service.SmsMessageSender;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
-@ImportResource("classpath:/beans.xml")
 public class MainConfig {
+    @Bean("koreanGreeter")
+    public MessageSender emailMessageSender(){
+        return new EmailMessageSender();
+    }
+
+    @Bean("englishGreeter")
+    public MessageSender smsMessageSender(){
+        return new SmsMessageSender();
+    }
 }
